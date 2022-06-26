66fae0ae1a89b4b5062520b6c29ab8e1d902b338
NneomaN <77930192+NneomaN@users.noreply.github.com>
2022-06-26 22:52:42 +0100

    Update Pizza_Runner_Dataset.sql

--- Case Study #2 - Pizza Runner/Pizza_Runner_Dataset.sql
+++ Case Study #2 - Pizza Runner/Pizza_Runner_Dataset.sql
@@ -43,11 +43,12 @@ VALUES
   ('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
 
 
+
 DROP TABLE IF EXISTS pizza_runner.runner_orders;
 CREATE TABLE pizza_runner.runner_orders (
   "order_id" INTEGER,
   "runner_id" INTEGER,
-  "pickup_time" VARCHAR(19),
+  "pickup_time" DATETIME,
   "distance" VARCHAR(7),
   "duration" VARCHAR(10),
   "cancellation" VARCHAR(23)
@@ -61,10 +62,10 @@ VALUES
   ('3', '1', '2020-01-03 00:12:37', '13.4km', '20 mins', NULL),
   ('4', '2', '2020-01-04 13:53:03', '23.4', '40', NULL),
   ('5', '3', '2020-01-08 21:10:57', '10', '15', NULL),
-  ('6', '3', 'null', 'null', 'null', 'Restaurant Cancellation'),
+  ('6', '3', null, 'null', 'null', 'Restaurant Cancellation'),
   ('7', '2', '2020-01-08 21:30:45', '25km', '25mins', 'null'),
   ('8', '2', '2020-01-10 00:15:02', '23.4 km', '15 minute', 'null'),
-  ('9', '2', 'null', 'null', 'null', 'Customer Cancellation'),
+  ('9', '2', null, 'null', 'null', 'Customer Cancellation'),
   ('10', '1', '2020-01-11 18:50:20', '10km', '10minutes', 'null');
 
 
