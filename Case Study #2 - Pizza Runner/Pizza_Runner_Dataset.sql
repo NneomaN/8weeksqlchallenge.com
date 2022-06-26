6c03684bf0b9d746cf2b902639afd4a3ad5e6638
NneomaN <77930192+NneomaN@users.noreply.github.com>
2022-06-26 22:25:02 +0100

    Create Pizza_Runner_Dataset.sql

---
+++ Case Study #2 - Pizza Runner/Pizza_Runner_Dataset.sql
@@ -0,0 +1,43 @@
+CREATE SCHEMA pizza_runner;
+
+DROP TABLE IF EXISTS pizza_runner.runners;
+CREATE TABLE pizza_runner.runners (
+  "runner_id" INTEGER,
+  "registration_date" DATE
+);
+INSERT INTO pizza_runner.runners
+  ("runner_id", "registration_date")
+VALUES
+  (1, '2021-01-01'),
+  (2, '2021-01-03'),
+  (3, '2021-01-08'),
+  (4, '2021-01-15');
+
+
+DROP TABLE IF EXISTS pizza_runner.customer_orders;
+CREATE TABLE pizza_runner.customer_orders (
+  "order_id" INTEGER,
+  "customer_id" INTEGER,
+  "pizza_id" INTEGER,
+  "exclusions" VARCHAR(4),
+  "extras" VARCHAR(4),
+  "order_time" DATETIME
+);
+
+INSERT INTO pizza_runner.customer_orders
+  ("order_id", "customer_id", "pizza_id", "exclusions", "extras", "order_time")
+VALUES
+  ('1', '101', '1', '', '', '2020-01-01 18:05:02'),
+  ('2', '101', '1', '', '', '2020-01-01 19:00:52'),
+  ('3', '102', '1', '', '', '2020-01-02 23:51:23'),
+  ('3', '102', '2', '', NULL, '2020-01-02 23:51:23'),
+  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+  ('4', '103', '2', '4', '', '2020-01-04 13:23:46'),
+  ('5', '104', '1', 'null', '1', '2020-01-08 21:00:29'),
+  ('6', '101', '2', 'null', 'null', '2020-01-08 21:03:13'),
+  ('7', '105', '2', 'null', '1', '2020-01-08 21:20:29'),
+  ('8', '102', '1', 'null', 'null', '2020-01-09 23:54:33'),
+  ('9', '103', '1', '4', '1, 5', '2020-01-10 11:22:59'),
+  ('10', '104', '1', 'null', 'null', '2020-01-11 18:34:49'),
+  ('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
