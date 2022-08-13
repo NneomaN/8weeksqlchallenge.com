49a18633f6e1821d6788adafc8a641b78c081429
Nneoma N <77930192+NneomaN@users.noreply.github.com>
2022-08-13 11:53:05 +0100

    Add files via upload

--- Case Study #2 - Pizza Runner/Pizza_Runner- Pizza Metrics - Solutions.sql
+++ Case Study #2 - Pizza Runner/Pizza_Runner- Pizza Metrics - Solutions.sql
@@ -1,110 +1,117 @@
 
 -- Q1 How many pizzas were ordered?
-select count(co.order_id) as pizzas_ordered
-from pizza_runner.customer_orders co;
+select count(order_id) as pizzas_ordered
+from pizza_runner.customer_orders_clean;
 
 -- Q2 How many unique customer orders were made?
-select count(distinct co.order_id) as orders_made
-from pizza_runner.customer_orders co;
+select count(distinct order_id) as orders_made
+from pizza_runner.customer_orders_clean;
 
 -- Q3 How many successful orders were delivered by each runner?
-/* 
--- Need to udate empty values to enssure accurate results everytime
-UPDATE pizza_runner.runner_orders
-SET cancellation = NULL
-WHERE cancellation = '' or cancellation = 'null' 
-*/
-
-select count(runner_id) successful_orders
-from pizza_runner.runner_orders
-where cancellation is null;
+select
+    runner_id
+	, count(runner_id) successful_orders
+from pizza_runner.runner_orders_clean
+where cancellation is null
+group by runner_id;
 
 -- Q4 How many of each type of pizza was delivered?
-
-/*
--- had to change column type to enabble grouping as text type cannot be sorted
-alter table pizza_runner.pizza_names
-alter column pizza_name varchar(25);
-*/
-
 select
     pn.pizza_name,
 	count(co.order_id) orders_made
-from pizza_runner.customer_orders as co
+from pizza_runner.customer_orders_clean as co
      inner join
 	 pizza_runner.pizza_names pn on co.pizza_id = pn.pizza_id
+	 inner join
+	 pizza_runner.runner_orders_clean ro on co.order_id = ro.order_id
+where ro.cancellation is null
 group by pn.pizza_name;
 
 -- Q5 How many Vegetarian and Meatlovers were ordered by each customer?
+with orders as
+(
 select
-    co.customer_id,
-	pn.pizza_name,
-	count(pn.pizza_id) oders_made
+    order_id
+	, customer_id
+	, co.pizza_id
+	, pizza_name
 from
-    pizza_runner.customer_orders as co
-	left join
+    pizza_runner.customer_orders_clean as co
+	join
 	pizza_runner.pizza_names as pn on co.pizza_id = pn.pizza_id
-group by co.customer_id, pn.pizza_name
-order by co.customer_id;
+)
+select
+    customer_id
+	, sum(Meatlovers) as Meatlovers
+	, sum(Vegetarian) as Vegetarian
+from orders
+pivot
+    (
+	count(order_id)
+	for pizza_name in (Meatlovers, Vegetarian)
+	) 
+	as pvt_pizza_names
+group by customer_id;
 
 -- Q6 What was the maximum number of pizzas delivered in a single order?
 with pizzas_by_order 
 as
-    (
-	select
-	    co.order_id
-		, count(co.pizza_id) pizzas_ordered
-	from pizza_runner.customer_orders as co
-	group by co.order_id
-	)
+(
+select
+    co.order_id
+	, count(co.pizza_id) pizzas_delivered
+from
+    pizza_runner.customer_orders_clean as co
+	inner join
+	pizza_runner.runner_orders_clean ro on co.order_id = ro.order_id
+where ro.cancellation is null
+group by co.order_id
+)
 
-select max(pizzas_ordered) as max_pizzas_in_order
+select max(pizzas_delivered) as max_pizzas_delivered
 from pizzas_by_order;
 
 -- Q7 For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
-/* 
--- Need to udate empty values to enssure accurate results everytime
-UPDATE pizza_runner.customer_orders
-SET exclusions = NULL
-WHERE exclusions = '' or exclusions = 'null'
 
-UPDATE pizza_runner.customer_orders
-SET extras = NULL
-WHERE extras = '' or extras = 'null'
-*/
-
-/* WIP
-with customer_changes 
+with delivered_pizzas 
 as
     (
-	select
-	    co.customer_id
-		, count(co.pizza_id) pizzas_ordered
-		, count(co.extras) as extras
-		, count(co.exclusions) as exclusions
-	from pizza_runner.customer_orders as co
-	group by co.customer_id
+	select co.order_id,
+	customer_id,
+	case when exclusions is null and extras is null then 'pizzas_without_changes'
+			else 'pizzas_with_changes'
+	end as [change]
+	from
+	    pizza_runner.customer_orders_clean as co
+		join
+		pizza_runner.runner_orders_clean as ro on co.order_id = ro.order_id
+	where cancellation is null
 	)
 
-select
-    customer_id
-	, change
-	, no_changes
-from customer_changes as cc
-unpivot
-    (no_changes for change in
-	   (extras, exclusions)
-	   ) as unpvt;
-*/
+SELECT customer_id, pizzas_with_changes, pizzas_without_changes
+FROM delivered_pizzas 
+PIVOT 
+( 
+count(order_id) FOR [change] IN (pizzas_with_changes, pizzas_without_changes) 
+) AS PivotTable;
 
 -- Q8 How many pizzas were delivered that had both exclusions and extras?
---WIP
+select count(pizza_id) pizzas_with_exc_and_ext
+from
+    pizza_runner.customer_orders_clean as co
+	join
+	pizza_runner.runner_orders_clean as ro on co.order_id = ro.order_id
+where cancellation is null and (exclusions is not null and extras is not null)
+
 
 -- Q9 What was the total volume of pizzas ordered for each hour of the day?
 select
-    datename(hh, order_time) as hour_of_day
+    concat(
+	    'Hour ',
+		datename(hh, order_time)
+		) as hour_of_day
 	, count(order_id) pizzas_ordered
-from pizza_runner.customer_orders
+from pizza_runner.customer_orders_clean
 group by datename(hh, order_time);
 
 -- Q10 What was the volume of orders for each day of the week?
@@ -113,7 +120,7 @@ select
     datepart(dw, order_time) as day_no
     , datename(dw, order_time) as day_of_week
 	, count(order_id) pizzas_ordered
-from pizza_runner.customer_orders
+from pizza_runner.customer_orders_clean
 group by 
     datepart(dw, order_time)
 	, datename(dw, order_time)
---
+++ Case Study #2 - Pizza Runner/Pizza_Runner- Runner and Customer Experience - Solutions.sql
@@ -0,0 +1,119 @@
+---- Q1 How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+
+-- First we need to define the week range starting from whatever day Jan 1st falls on
+
+-- create variable to hold fisrt day of week value
+declare @dw_no int
+set @dw_no = datepart(dw, '2021-01-01');
+
+-- set first day of week to 
+set datefirst @dw_no;
+
+-- find runners who registered in each 7 day period
+select
+    concat(
+	    'Week ',
+		datepart(wk, registration_date)
+		) as week_no
+	, count(runner_id) runners
+from pizza_runner.runners
+group by datepart(wk, registration_date)
+
+-- return first day of week to default
+set datefirst 1;
+
+
+---- Q2 What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+with pickup_mins as
+(
+select 
+    distinct ro.order_id
+	, ro.runner_id 
+	, datediff(minute, co.order_time, ro.pickup_time) as minutes_to_pickup
+from
+    pizza_runner.customer_orders_clean as co
+	right join
+	pizza_runner.runner_orders as ro on co.order_id = ro.order_id
+where ro.pickup_time is not null
+)
+
+select
+    runner_id
+	, avg(minutes_to_pickup) average_pickup_minutes
+from pickup_mins
+group by runner_id;
+
+-- Q3 Is there any relationship between the number of pizzas and how long the order takes to prepare?
+with pickup_mins as
+(
+select 
+    ro.order_id
+	, ro.runner_id 
+	, datediff(minute, co.order_time, ro.pickup_time) as minutes_to_pickup
+from
+    pizza_runner.customer_orders_clean as co
+	right join
+	pizza_runner.runner_orders as ro on co.order_id = ro.order_id
+where ro.pickup_time is not null
+)
+
+select
+    pm.order_id
+	, count(pm.order_id) pizzas
+	, avg(pm.minutes_to_pickup) avg_pickup_mins
+from pickup_mins as pm
+group by pm.order_id;
+
+--- Q4 What was the average distance travelled for each customer?
+select 
+    co.customer_id
+	, round(avg(ro.distance_km), 2) as avg_distance_travelled
+from
+    pizza_runner.customer_orders_clean as co
+	right join
+	pizza_runner.runner_orders_clean as ro on co.order_id = ro.order_id
+where ro.cancellation is NULL
+group by co.customer_id;
+
+-- Q5 What was the difference between the longest and shortest delivery times for all orders?
+select max(duration_mins)-min(duration_mins) as duration_range
+from pizza_runner.runner_orders_clean;
+
+-- Q6 What was the average speed for each runner for each delivery and do you notice any trend for these values?
+select
+    runner_id
+	, order_id
+	, avg(duration_mins) avg_duration
+from pizza_runner.runner_orders_clean
+where cancellation is null
+group by runner_id, order_id
+order by runner_id, order_id;
+
+-- riders appear to be getting faster
+
+---- Q7 What is the successful delivery percentage for each runner?
+with all_orders as
+(
+select
+    runner_id
+	, count(distinct order_id) as all_orders
+from pizza_runner.runner_orders_clean
+group by runner_id
+),
+successful_orders as
+(
+select
+    runner_id
+	, count(distinct order_id) as successful_orders
+from pizza_runner.runner_orders_clean
+where cancellation is null
+group by runner_id
+)
+ 
+select
+    ao.runner_id
+	, concat((successful_orders*100)/all_orders, '%') as perc_successful_orders
+from 
+    all_orders as ao
+	join
+	successful_orders as so on ao.runner_id = so.runner_id
